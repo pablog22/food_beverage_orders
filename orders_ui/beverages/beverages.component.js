f802cc1e402f15772301670bb7e358d678bba8c0
Pablo Grasso <pgrasso22@gmail.com>
2017-02-23 19:23:21 -0300

    Add +  - and x buttons

--- orders_ui/beverages/beverages.component.js
+++ orders_ui/beverages/beverages.component.js
@@ -14,6 +14,33 @@
         $http.get('beverages/beverages.json').then(function(response) {
             self.products = response.data;
         });
+
+        this.calculateProductPurchaseTotal = function(product) {
+            product.purchaseTotal = product.purchaseCount * product.price;
+        }
+
+        this.addToCart = function(product) {
+            if (!product.purchaseCount) {
+                product.purchaseCount = 1;
+            } else {
+                product.purchaseCount += 1;
+            }
+
+            this.calculateProductPurchaseTotal(product);
+        };
+
+        this.removeFromCart = function(product) {
+            if (product.purchaseCount > 0) {
+                product.purchaseCount -= 1;
+            }
+
+            this.calculateProductPurchaseTotal(product);
+        };
+
+        this.removeAllFromCart = function(product) {
+            product.purchaseCount=0;
+            this.calculateProductPurchaseTotal(product);
+        };
     };
 
 
